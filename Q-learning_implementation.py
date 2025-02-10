cdffa5643346731981d77c4ac39c702392a7c1b4
Justin Harrott <Sir.Juggeth@Gmail.com>
2025-02-10 17:43:49 -0500

    new day

--- Q-learning_implementation.py
+++ Q-learning_implementation.py
@@ -1,4 +1,5 @@
-'''
+"""
+===========================================
 Q-learning update rule:
 Q(s_t, a_t) = Q(s_t, a_t) + alpha * (r_t+1 + gamma * max_a Q(s_t+1, a) - Q(s_t, a_t))
 
@@ -8,9 +9,16 @@ Where:
 - r_t+1: Reward received after taking action a_t in state s_t
 - gamma: Discount factor (0 <= gamma <= 1)
 - max_a Q(s_t+1, a): Maximum Q-value for the next state s_t+1
-'''
+===========================================
+"""
 
 
+
+"""
+===========================================
+Imports
+===========================================
+"""
 import gymnasium as gym
 import ale_py
 import numpy as np
@@ -18,21 +26,15 @@ import matplotlib.pyplot as plt
 import time
 from IPython.display import clear_output
 
-plt.ion()  # Enable interactive mode
-
 from gymnasium.envs.registration import register
 
 
-state_size = env.observation_space.n #In a 4x4 grid, each position (or cell) in the grid is a state. Since there are 4 rows 
-# and 4 columns, the total number of states is: 4 * 4, So, there are 16 states in a 4x4 grid.
-action_size = env.action_space.n # (actions are 0-3: left, down, right, up ∴ size is 4)
-# Rows are States, columns are Actions.
-q_table = np.zeros([state_size, action_size]) # creates 16x4 array of 0s
-'''
-Each state (square on the board) has its own row, and the actions (to move in a different direction) are represented as columns 
-in the Q-table. The Q-values in the table tell you the expected reward for each action taken from each square.
-'''
 
+"""
+===========================================
+Global Constants and Configurations
+===========================================
+"""
 EPOCHS = 20000 # same as EPISODES - how many times the agent plays the game.
 ALPHA = 0.8 # same as LEARNING RATE - should be close to, but not equal to 1. Set too low and will take too long to learn, 
 # set too high and it may never converge bacause it causes unstable, overly reactive Q-values, preventing convergence due 
@@ -42,14 +44,37 @@ GAMMA = 0.95 # same as DISCOUNT RATE - should be close to, but not equal to one.
 # makes it short-sighted, ignoring long-term rewards and converging to suboptimal policies. Depends on your environment 
 # for which side you lean to. Good way is to use exponential decay function, but a linear decrease of a certain vlue is common.
 
+env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=False, render_mode="human")
+
+state_size = env.observation_space.n #In a 4x4 grid, each position (or cell) in the grid is a state. Since there are 4 rows 
+# and 4 columns, the total number of states is: 4 * 4, So, there are 16 states in a 4x4 grid.
+action_size = env.action_space.n # (actions are 0-3: left, down, right, up ∴ size is 4)
+# Rows are States, columns are Actions.
+q_table = np.zeros([state_size, action_size]) # creates 16x4 array of 0s
+'''
+Each state (square on the game board) has its own row in the q_table representating the reward vulues for each action (to move in a 
+different direction), they are sahowing as columns in the q_table. The Q-values in the table tell you the expected reward for each 
+action taken from each square.
+'''
 epsilon = 1.0 # Exploration vs exploitation parameter. Usually starts at 1 (exploration) and uses a function to decrease 
 # it as epochs are run. 
 max_epsilon = 1.0
 min_epsilon = 0.01
 decay_rate = 0.001
 
+rewards = []
+
+plt.ion()  # Enable interactive plot mode
+
+
+
+"""
+===========================================
+Functions and Utility Definitions
+===========================================
+"""
 def epsilon_greedy_action_selection(epsilon, q_table, discrete_state):
-    random_number = np.random.random()
+    random_number = np.random.random() # random number between 0-0.1 (non-inclusive)
     
     # EXPLOITATION (choose the action that maximizes Q) as epsilon gets smaller and smaller, the function will get greedier and greedier
     # going with what it has found has a better reward
@@ -68,8 +93,6 @@ def compute_next_q_value(old_q_value, reward, next_optimal_q_value):
     
 def reduce_epsilon(epsilon, epoch):
     return min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay_rate * epoch) # exponential decay
-
-
 
 '''
 def simple_agent(observation):
@@ -92,10 +115,16 @@ def simple_agent(observation):
 '''
 
 
+
+"""
+===========================================
+Main Logic / Execution Block
+===========================================
+"""
 def main():
     try:
         register(
-            id='FrozenLakeNotSlippery-v1',  # Unique environment ID
+            id='FrozenLake-v1',  # Unique environment ID
             entry_point='gymnasium.envs.toy_text:FrozenLakeEnv',  # Path to the environment class
             max_episode_steps=100  # Optional: max steps per episode
             #reward_threshold=0.78,  # Optional: reward threshold for success. Optimum for FrozenLake is 0.8196
@@ -103,9 +132,9 @@ def main():
     except:
         print('Registration name already taken')
 
-
+    '''
+    # basic script to see the game work
     # Initialise the environment
-    env = gym.make('FrozenLakeNotSlippery-v1', desc=None, map_name="4x4", is_slippery=False, render_mode="human")
     env.reset()
 
     # Reset the environment to generate the first observation OBSERVATION is same as STATE
@@ -127,6 +156,54 @@ def main():
         # If the episode has ended then we can reset to start a new episode
         if terminated or truncated:
             observation, info = env.reset()
+
+    env.close()
+    '''
+
+    # training_loop
+    log_interval = 200
+
+    for episode in range(EPOCHS):
+
+        global epsilon 
+        state = env.reset()
+        print(state)
+        done = False
+        total_rewards = 0
+
+        while not done:
+            # ACTION
+            action = epsilon_greedy_action_selection(epsilon, q_table, state)
+
+            # state, reward... env.step
+            new_state, reward, terminated, truncated, info = env.step(action) # new_state is same as observation
+            print(new_state, reward, terminated, truncated, info, done)
+
+            # OLD (current) Q-value table Q(s_t, a_t)
+            old_q_value = q_table[state, action]
+
+            # get next optimal Q-value (max Q for this state) Q(s_t+1, a_t+1)
+            next_optimal_q_value = np.max(q_table[new_state,:])
+
+            # compute the next Q-value
+            next_q = compute_next_q_value(old_q_value, reward, next_optimal_q_value)
+
+            # Update the Q-value table
+            q_table[state, action] = next_q
+            
+            # track rewards
+            total_rewards = total_rewards + reward
+
+            # new state is now the state
+            state = new_state
+
+        # Agent finished a round of the game
+        episode += 1
+        epsilon = reduce_epsilon(epsilon, episode)
+        rewards.append(total_rewards)
+
+        if episode % log_interval == 0:
+            print.np.sum[rewards]
 
     env.close()
 
